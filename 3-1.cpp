54a1a378398daa549c6333b84abfae301a8d9eec
111J531 <100131324+111J531@users.noreply.github.com>
2022-03-14 20:29:54 +0800

    Create 3-1.cpp

---
+++ 3-1.cpp
@@ -0,0 +1,59 @@
+
+#include <stdio.h>
+
+int main() {
+    int a, b, product;
+    a = 20;
+    b = 7;
+
+ 
+ /*
+ 
+ x + y
+x - y
+x * y
+x / y
+x % y
+
+ 
+ */
+ 
+ 
+    // Calculating product
+    product = a + b;
+
+ 
+    printf("a * b = %d \n", product);
+    
+    product = a - b;
+
+ 
+    printf("a * b = %d \n", product);
+    
+    
+    /*
+ 
+
+++x
+x++
+--x
+x--
+
+
+ 
+ */
+ 
+ 
+ /*
+ 
+
+
+x = 16 
+x >> 1
+x >> 2
+x << 1
+ 
+ */
+    
+    return 0;
+}
