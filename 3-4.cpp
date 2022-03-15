ed0249ea6f6a3f6894b851603c4394697a97f116
111J531 <100131324+111J531@users.noreply.github.com>
2022-03-15 20:31:31 +0800

    Update 3-4.cpp

--- 3-4.cpp
+++ 3-4.cpp
@@ -1,14 +1,32 @@
-##include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int x=10;
+    int x=19;
     
     //x = x + 5;
     x+=5 ;
     printf("x= %d \n ", x);
     //x = x - 3
-    x-=3
+    x-=3;
+    printf("x= %d \n ", x);
+    //x = x * 3
+    x*=3;
+    printf("x= %d \n ", x);
+    //x = x / 3
+    x/=3;
+    printf("x= %d \n ", x);
+    //x = x % 3
+    x%=3;
+    printf("x= %d \n ", x);
+    //x = x & 3
+    x&=3;
+    printf("x= %d \n ", x);
+    //x = x | 3
+    x|=3;
+    printf("x= %d \n ", x);
+    //x = x ^ 3
+    x^=3;
     printf("x= %d \n ", x);
     return 0;
 }
