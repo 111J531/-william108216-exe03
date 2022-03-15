dd1874e62d02b8e141ec806a1ac348d7d146f7cd
111J531 <100131324+111J531@users.noreply.github.com>
2022-03-15 20:25:36 +0800

    Create 3-4.cpp

---
+++ 3-4.cpp
@@ -0,0 +1,14 @@
+##include <stdio.h>
+
+int main()
+{
+    int x=10;
+    
+    //x = x + 5;
+    x+=5 ;
+    printf("x= %d \n ", x);
+    //x = x - 3
+    x-=3
+    printf("x= %d \n ", x);
+    return 0;
+}
