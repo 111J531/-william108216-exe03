e3eaec5cb737aa17592fe17286f192ddd2693973
111J531 <100131324+111J531@users.noreply.github.com>
2022-03-21 19:12:27 +0800

    Update 3-4.cpp

--- 3-4.cpp
+++ 3-4.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     int x=19;
+    printf("input x= %d \n ", x);
     
     //x = x + 5;
     x+=5 ;
@@ -27,6 +28,14 @@ int main()
     printf("x= %d \n ", x);
     //x = x ^ 3
     x^=3;
+    printf("x= %d \n ", x);
+    return 0;
+    //x = x >> 3
+    x>>=3;
+    printf("x= %d \n ", x);
+    return 0;
+     //x = x << 3
+    x<<=3;
     printf("x= %d \n ", x);
     return 0;
 }
